7aa5ce57bdc9caad981c911889ebdca6af29d4ba
lab_services_student <lab_services_student@lab7L95SR>
2025-06-24 20:12:03 +0200

    Added comments and fixed the css for entire website

--- AgriEnergy/Program.cs
+++ AgriEnergy/Program.cs
@@ -14,8 +14,6 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-
-
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -61,56 +59,3 @@ app.Run();
 
 
 
-
-
-
-
-
-
-
-
-/*using AgriEnergy.Data;
-using AgriEnergy.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDefaultIdentity<ApplicationUser>()
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-
-
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Seed the DB
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    await DBInitializer.Seed(services);
-}
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();*/
